5751f1795995f90a0d40a0b6f725941c3a864e78
xudawu201 <xudawu201@icloud.com>
2024-11-26 08:54:38 +0800

    upload file

--- SICAU_JWC_2024_09_20/docker_stop_remove_build_run.bat
+++ SICAU_JWC_2024_09_20/docker_stop_remove_build_run.bat
@@ -1,5 +1,5 @@
 docker rm  -f sicau_jwc_container
 docker rmi -f sicau_jwc_image
 
-docker build -t sicau_jwc_image:v20241120 .
-docker run --restart always --name sicau_jwc_container -p 8000:8000 -d sicau_jwc_image:v20241120
+docker build -t sicau_jwc_image:v20241126 .
+docker run --restart always --name sicau_jwc_container -p 8000:8000 -d sicau_jwc_image:v20241126
